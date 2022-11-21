c3a39f365fce8b24eba68ba65f9d088c2c118caf
Jonas Dickel <j.dickel@adscape.de>
2022-11-21 21:21:02 +0100

    initial commit

---
+++ src/main/java/me/jdickel/articleexplorer/ArticleExplorerApplication.java
@@ -0,0 +1,13 @@
+package me.jdickel.articleexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jdickel/articleexplorer/ArticleExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jdickel.articleexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
