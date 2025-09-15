863685482beba2460b2d6d83bf250307b6f8af2d
Jonas Dickel <jd@jddesigns.de>
2025-09-15 23:30:02 +0200

    add initial login page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,14 @@
+import Login from '@/views/Login.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+        path: "/login",
+        component: Login
+    }
+  ],
 })
 
 export default router
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="full-container h-100">
+        <div class="col-4">
+            <div class="container d-flex flex-column">
+                <div class="logo">
+                    Logo here
+                </div>
+                <div class="form">
+                    Form here
+                </div>
+                <div class="copyright">
+                    Copyright
+                </div>
+            </div>
+        </div>
+        <div class="col text-center">
+            IMAGE
+        </div>
+    </div>
+</template>
